cc8fb5811c89736fa6424338432633f184e3d969
Rongcui Dong <rongcuid@outlook.com>
2017-02-18 18:40:14 -0800

    Save Progress

--- src/ctags.sh
+++ src/ctags.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 find ../ChibiOS-src -type f -iname "*.[chS]" | grep -v MSP | grep -v TIVA | grep -v STM32 | grep -v SPC5 | grep -v LPC | grep -v KINETIS | grep -v test | xargs ctags -a
+echo | avr-gcc -E -Wp,-v - 2>&1 | egrep '^ ' | xargs ctags -a
+
