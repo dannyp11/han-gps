6e9acb390752406ae25a89db804536c0b02ac99d
Rongcui Dong <rongcuid@outlook.com>
2017-02-19 23:58:18 -0800

    Slight update

--- ctags.sh
+++ ctags.sh
@@ -6,5 +6,5 @@ DIRS=`echo | avr-gcc -E -Wp,-v - 2>&1 | egrep '^ '`
 DIRS+=`printf "\nChibiOS-src"`
 rm -f tags
 for d in $DIRS ; do
-	find $d -type f -iname "*.[chS]" | grep -v CMSIS | grep -v e200 | grep -v ARM | grep -v SIMIA32 | grep -v NRF5 | grep -v MSP | grep -v TIVA | grep -v STM32 | grep -v SPC5 | grep -v LPC | grep -v KINETIS | grep -v test | xargs ctags -a
+	find $d -type f -iname "*.[chS]" | grep -v CMSIS | grep -v e200 | grep -v ARM | grep -v SIMIA32 | grep -v NRF5 | grep -v MSP | grep -v TIVA | grep -v SPC5 | grep -v LPC | grep -v KINETIS | grep -v test | xargs ctags -a
 done
