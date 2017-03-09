a094975d1b6b98a90fcbe7bc2b84ddfdd91ab60c
DESKTOP-Q36ISQN\Kevin <kaycbas@outlook.com>
2017-03-09 12:49:03 -0800

    LED API done. Test program included.

--- test/led/testLED.c
+++ test/led/testLED.c
@@ -1,65 +1,40 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define SER_PORT    PORTD
-#define SER_PIN     2
-#define RCLK_PORT PORTD
-#define RCLK_PIN  3
-#define SRCLK_PORT PORTD
-#define SRCLK_PIN  4
- 
-#define SER_low()  SER_PORT&=~_BV(SER_PIN)
-#define SER_high() SER_PORT|=_BV(SER_PIN)
-#define RCLK_low()  RCLK_PORT&=~_BV(RCLK_PIN)
-#define RCLK_high() RCLK_PORT|=_BV(RCLK_PIN)
-#define SRCLK_low()  SRCLK_PORT&=~_BV(SRCLK_PIN)
-#define SRCLK_high() SRCLK_PORT|=_BV(SRCLK_PIN)
-
-//Define functions
-//===============================================
-void ioinit(void);
-void output_led_state(unsigned int __led_state);
-//===============================================
-
+#include "LED.h"
 
 
 int main(void) {
 	
 	ioinit(); //Setup IO pins and defaults
-	int i;
 
 	while (1) {
-			for (i=7;i>=0;i--)
-      {
-         output_led_state(_BV(i));
-         _delay_ms(200);
-      }
+			LEDall();
+      _delay_ms(300);
+     LEDclear();
+      _delay_ms(300);
+      LEDall();
+      _delay_ms(300);
+      LEDclear(); 
+      _delay_ms(300);
+      LEDnorth();
+      _delay_ms(300);
+      LEDnortheast();
+      _delay_ms(300);
+      LEDeast();
+      _delay_ms(300);
+      LEDsoutheast();
+      _delay_ms(300);
+      LEDsouth();
+      _delay_ms(300);
+      LEDsouthwest();
+      _delay_ms(300);
+      LEDwest();
+      _delay_ms(300);
+      LEDnorthwest();
+      _delay_ms(300);
 	}
 
 	return 0; /* never reached */
 }
 
-void ioinit (void)
-{
-    DDRD  = 0b00011100; //1 = output, 0 = input
-    PORTD = 0b00000000;
-}
-
-void output_led_state(unsigned int __led_state)
-{
-   SRCLK_low();
-   RCLK_low();
-   int i;
-   for (i=0;i<8;i++)
-   {
-      if (bit_is_set(__led_state, i))
-         SER_high();
-      else   
-         SER_low();
- 
- 
-      SRCLK_high();
-      SRCLK_low();
-   }
-   RCLK_high();
-}
